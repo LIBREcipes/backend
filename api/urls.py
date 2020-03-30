3ed653f87a779e55c178812d48dec268b9ccd3e6
Matthias De Bie <mattydebie@gmail.com>
2020-03-30 23:11:22 +0200

    added jwt

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+
 from . import views
 
 
@@ -12,5 +19,8 @@ router.register(r'user', views.UserViewset)
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 
 from .serializers import RecipeSerializer, IngredientSerializer, UserSerializer, RecipeIngredientSerializer
@@ -8,17 +9,21 @@ from .models import Recipe, Ingredient, RecipeIngredient
 class RecipeViewset(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class IngredientViewset(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
 
 class RecipeIngredientViewset(viewsets.ModelViewSet):
     queryset = RecipeIngredient.objects.all()
     serializer_class = RecipeIngredientSerializer
+    permission_classes = (IsAuthenticated,)
