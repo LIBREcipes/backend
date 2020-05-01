b2ba8d1cd1a8d0ddc7969d388d15aadc41051729
Matthias De Bie <mattydebie@gmail.com>
2020-05-01 15:52:30 +0200

    add sorting to recipe ingredient

--- librecipes/settings.py
+++ librecipes/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^68jw2hq_=v44xhz7=pw-%odaq+w=7)-xh8qobalp^ib%!l_x^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
